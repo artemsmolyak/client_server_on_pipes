b6852c7da6992a90968b70353583b7873ded0c26
artyom <artem.smolet@gmail.com>
2022-07-06 15:47:31 +0300

    Initial Commit

---
+++ pipes/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  pipes
+//
+//  Created by Artyom on 06.07.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
